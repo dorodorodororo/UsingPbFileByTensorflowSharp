a1d7622ae7408ce0c67099590c243159500b6113
dorodorodororo <74817754+dorodorodororo@users.noreply.github.com>
2021-06-01 00:35:11 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TensorflowSharp_Inception_Final.TensorflowSharp_Inception_Final_1 p1 = new TensorflowSharp_Inception_Final.TensorflowSharp_Inception_Final_1();
+            p1.Run();
+            Console.ReadLine();
+        }
+    }
+}
